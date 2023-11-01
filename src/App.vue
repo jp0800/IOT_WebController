47a6609cbca193af6ce4b0e2ba6b131c4f838d71
jp0800 <angelthis.john@gmail.com>
2023-11-01 15:46:09 +0800

    :+1: Added App Component

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+import HelloWorld from '@/components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <i class="bi bi-bezier2 logo green"></i>
+
+    <div class="wrapper">
+      <HelloWorld msg="IoT Web Controller" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  text-align: center;
+  margin: 0 auto 2rem;
+  font-size: 8rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
